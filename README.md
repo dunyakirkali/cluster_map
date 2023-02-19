497e634682eeee6cb6e1f315340904f9a71a0269
Dunya Kirkali <dunya.kirkali@booking.com>
2023-02-19 21:53:40 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ClusterMap
 
-**TODO: Add description**
+1. Break up the large array into smaller chunks that can be processed by each node.
+2. Spawn an Elixir process on each machine that will be processing a chunk of the array. Each process should have a unique name so that they can communicate with each other.
+3. Send the chunks of the array to each process for processing.
+4. Have each process sum its chunk of the array and send the result back to the parent process.
+5. The parent process can then sum the results from each child process to get the total sum of the array.
 
 ## Installation
 
--- lib/cluster_map.ex
+++ lib/cluster_map.ex
@@ -1,18 +1,59 @@
 defmodule ClusterMap do
-  @moduledoc """
-  Documentation for `ClusterMap`.
-  """
+  def sum_clustered(values, num_nodes) do
+    chunk_size = div(length(values), num_nodes)
 
-  @doc """
-  Hello world.
+    chunks_and_nodes = create_chunks_and_nodes(values, chunk_size, num_nodes)
 
-  ## Examples
+    nodes = start_nodes(chunks_and_nodes)
 
-      iex> ClusterMap.hello()
-      :world
+    results = collect_results(nodes)
 
-  """
-  def hello do
-    :world
+    Enum.sum(results)
+  end
+
+  def create_chunks_and_nodes(values, chunk_size, num_nodes) do
+    for n <- 1..num_nodes do
+      chunk = Enum.slice(values, (n - 1) * chunk_size, chunk_size)
+
+      node = spawn_node()
+
+      {chunk, node}
+    end
+  end
+
+  defp spawn_node do
+    node_name = :"node#{:rand.uniform(1_000_000_000)}"
+
+    Node.spawn(node_name, &node_loop/0)
+
+    node_name
+  end
+
+  defp node_loop do
+    receive do
+      {:process_chunk, chunk} ->
+        sum = Enum.sum(chunk)
+
+        send(self(), {:sum_result, sum})
+
+        node_loop()
+    end
+  end
+
+  defp start_nodes(chunks_and_nodes) do
+    Enum.map(chunks_and_nodes, fn {chunk, node} ->
+      send(node, {:process_chunk, chunk})
+
+      node
+    end)
+  end
+
+  defp collect_results(nodes) do
+    Enum.map(nodes, fn node ->
+      receive do
+        {:sum_result, sum} ->
+          sum
+      end
+    end)
   end
 end
